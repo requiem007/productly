87d5e1a4e860da20d4c936b643b4e8893043ba9c
requiem007 <requiem007@ukr.net>
2023-01-08 21:05:30 +0200

    some working

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import { Article } from "./Article";
+
 // ===================бургер========================================================================================================================================
 
 const burgerIcon = document.querySelector(".icon-menu");
@@ -11,7 +13,7 @@ if (burgerIcon) {
     });
 }
 
-//плавная прокрустка по клику
+//плавная прокрустка по клику ==============================================================================================
 
 const menuLinks = document.querySelectorAll(".nav__link[data-goto]"); //собрал в массив все ссылки с дата-атрибутов data-goto
 if (menuLinks.length > 0) {
@@ -37,3 +39,56 @@ function onMenuLinkClick(e) {
         e.preventDefault();
     }
 }
+
+//  табы ==============================================================================================
+
+window.onload = function () {
+    // Tags
+    addTagsClickHandler();
+};
+
+const addTagsClickHandler = () => {
+    document.querySelector(".tabs-strategies__nav").addEventListener("click", (e) => {
+        if (e.target.classList.contains("tabs-strategies__button")) {
+            let clickedButton = e.target;
+            removeActive();
+            addActive(clickedButton);
+            if (clickedButton.innerText === "All") {
+                showAllStrategies();
+            } else {
+                filterStrategyBySelectedButton(clickedButton.innerText);
+            }
+            // console.log(clickedButton.innerText);
+        }
+    });
+};
+
+const removeActive = () => {
+    let buttons = document.querySelectorAll(".tabs-strategies__button");
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+};
+
+const addActive = (clickedButton) => {
+    clickedButton.classList.add("active");
+};
+
+const showAllStrategies = () => {
+    let strategies = document.querySelectorAll(".tabs-strategies__items .tabs-strategies__item");
+    strategies.forEach((strategy) => {
+        strategy.classList.remove("hide");
+    });
+};
+
+const filterStrategyBySelectedButton = (selectedButton) => {
+    let strategies = document.querySelectorAll(".tabs-strategies__items .tabs-strategies__item");
+    strategies.forEach((strategy) => {
+        strategy.classList.add("hide");
+        strategy.querySelectorAll(".tabs-strategies__type").forEach((button) => {
+            if (button.innerText === selectedButton) {
+                strategy.classList.remove("hide");
+            }
+        });
+    });
+};
